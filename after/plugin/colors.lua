8029854cad2a869d365a7aefd8b98213348c44db
F0xedb <tom@odex.be>
2023-02-23 10:18:56 +0100

    Initial neovim configuration
    It includes an array of plugins:
    
    - Dark colorscheme which allows transparancy
    - Easy searching in git repo
    - LSP with multiple languages
    - ...
    
    Additionally a few usefull remapping of common actions are provided as
    well

---
+++ after/plugin/colors.lua
@@ -0,0 +1,16 @@
+--[[require('rose-pine').setup({
+    disable_background = true
+})]]--
+
+vim.cmd("colorscheme tokyonight-night")
+
+function ColorMyPencils(color)
+	color = color or "tokyonight-night"
+	vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+end
+
+ColorMyPencils()
